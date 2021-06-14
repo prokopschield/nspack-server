7086c48b44ba18fd40791bea03eb398a77ed9a55
Prokop <prokop.schield@protonmail.com>
2021-06-14 10:25:20 +0200

    Add REAL_HOST to config

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,32 @@ import { getConfig } from 'doge-config';
 import path from 'path';
 
 const config = getConfig('packager-server', {
+	/**
+	 * Externally accessable URI root
+	 */
+	REAL_HOST: 'http://localhost',
+	/**
+	 * Passed to nodesite.eu
+	 */
 	NODESITE_NAME: 'pkg',
+	/**
+	 * For local listener
+	 */
 	HTTP_PORT: 80,
+	/**
+	 * Where to mount tmpfs
+	 */
 	TMPFS_PATH: path.resolve('.', 'tmpfs'),
+	/**
+	 * tmpfs size
+	 */
 	TMPFS_SIZE: '1g',
-	UID: 1000,
-	GID: 1000,
+	/**
+	 * UID and GID to lower privileges to
+	 * Set to 0 to run as root
+	 */
+	UID: process.getuid() || 1000,
+	GID: process.getgid() || 1000,
 });
 
 export default config;
